8aeb693c9dcb3387517cedf073188d1e694b3d1c
martin-mcinerney <martin_mcinerney@live.ie>
2022-08-21 16:20:23 +0100

    Adds counting H1s test

--- dommock.test.js
+++ dommock.test.js
@@ -3,24 +3,25 @@
  */
 const {
     changeHeading,
-    button,
-    heading,
 } = require('./dommock');
 
 beforeAll(() => {
+
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
-    heading = require("/dommock".heading)
+    heading = require("./dommock").heading
 });
 
 
 
 describe("testing the DOM elements", () => {
-    test("testing that h1 text is toggled", () => {
-        changeHeading();
-        expect(document.getElementsByTagName("h1")).length.toBe(1)
+    test("check that there are 2 H1 elements", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(2)
+    });
+    test("check that the inner text of H1 is 'BEFORE CLICK'", () => {
+        expect(heading.innerText).toBe("BEFORE CLICK")
     });
 });
